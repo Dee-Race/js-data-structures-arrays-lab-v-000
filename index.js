ca98f0b8474bc0f6510ca4814909d681d411f06e
Dee Race <deerace12@gmail.com>
2018-07-27 18:49:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const drivers = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendDriver(name) {
   const newDrivers = drivers.push("Ralph")
-  
+
+}
+
+function destructivelyPrependDriver(name) {
+  const newDrivers = drivers.unshift("Ralph")
 }
